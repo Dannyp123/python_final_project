6ad7ba48614107a945d03e9f94644b432aa4f75e
Daniel <dpeterson@basecampcodingacademy.org>
2018-07-25 10:52:14 -0500

    Made a Inventory function in shell to start of and got it to start to print like I want it

--- shell.py
+++ shell.py
@@ -1,14 +1,57 @@
 import disk
 import core
 
+# def inventory_dictionary():
+inventory = {
+    1: {
+        'name': 'Hammer',
+        'stock': 17,
+        'rental cost': 24,
+        'replacement cost': 34
+    },
+    2: {
+        'name': 'Drill',
+        'stock': 10,
+        'rental cost': 55,
+        'replacement cost': 100
+    },
+    3: {
+        'name': 'Saw',
+        'stock': 18,
+        'rental cost': 20,
+        'replacement cost': 25
+    }
+}
+
+# return inventory
+
 
 def main():
-    inventory_raw_info = disk.open_file('inventory.txt')
-    inventory_dictionary = core.create_inventory_dictionary(inventory_raw_info)
+    # inventory_raw_info = disk.open_file('inventory.txt')
+    # inventory_dictionary = core.create_inventory_dictionary(inventory_raw_info)
+    inventory = {
+        '1': {
+            'name': 'Hammer',
+            'stock': 17,
+            'rental cost': 24,
+            'replacement cost': 34
+        },
+        '2': {
+            'name': 'Drill',
+            'stock': 10,
+            'rental cost': 55,
+            'replacement cost': 100
+        },
+        '3': {
+            'name': 'Saw',
+            'stock': 18,
+            'rental cost': 20,
+            'replacement cost': 25
+        }
+    }
 
     print("Welcome to Daniel's Tool Rental!")
 
-    print(inventory_dictionary)
     name = input("What is the name for this rental? ")
     who_are_you = input("Are you a Employee or a Customer? ").strip().title()
     while True:
@@ -18,10 +61,15 @@ def main():
             help = input("Would you like to rent a Tool or see our inventory? "
                          ).strip()
         if help in ['See our inventory', 'see our inventory']:
-            print(file_information)
+            print(inventory[1]['name'])
         if help in ['rent', 'rent a tool', 'rent a Tool']:
+            print(
+                inventory['1']['name'],
+                inventory['2']['name'],
+                inventory['3']['name'],
+            )
+
             tool = input('OK, what tool would you like to rent? ').strip()
-            print(inventory_dictionary)
             if tool in ['Hammer', 'hammer']:
                 print('Rental Cost for', tool, 'is $ 24.0')
             elif tool in ['Drill', 'drill']:
@@ -30,8 +78,9 @@ def main():
                 print('Rental Cost for', tool, 'is $ 15.0')
             elif tool in ['Screwdriver', 'screwdriver']:
                 print('Rental Cost for', tool, 'is $ 20.0')
-        # renting_tool = inventory[tool]
-        # renting_tool['In-stock'] -= 1
+
+    # renting_tool = inventory[tool]
+    # renting_tool['In-stock'] -= 1
 
 
 if __name__ == '__main__':
